9f0c4e9882184c6e04ebfdac339bc7f1255f3643
James Odeen <jodeen@gmail.com>
2023-12-21 05:56:35 -0700

    Day 21 first part

--- src/Days/Day21.hs
+++ src/Days/Day21.hs
@@ -12,7 +12,7 @@ import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text hiding (take)
 import Data.Void
 {- ORMOLU_ENABLE -}
 
@@ -20,19 +20,36 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
-inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = do
+    points <- (many1' (notChar '\n')) `sepBy1'` endOfLine
+    return (toLayout points)
+
+toLayout :: [String] -> Input
+toLayout points = Map.fromList (zip [(x,y) | y <- [0..(length points)-1], x <- [0..(length (head points))-1]] (concat points))
+
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Map Point Char
 
-type OutputA = Void
+type Point = (Int, Int)
+
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+neighbors :: Point -> [Point]
+neighbors (x,y) = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+
+expand :: Set Point -> [Point] -> [Point]
+expand map points = filter (`Set.member` map) (nub (concatMap neighbors points))
+
+-- partA :: Input -> OutputA
+partA map = length (nub (steps !! 64))
+    where 
+        Just (startPoint, _) = find (\(_, c) -> c == 'S') (Map.assocs map)
+        keys = Set.insert startPoint (Map.keysSet (Map.filter (== '.') map))
+        steps = iterate (expand keys) [startPoint]
 
 ------------ PART B ------------
 partB :: Input -> OutputB
